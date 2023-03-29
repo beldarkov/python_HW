e33c61efa504d3444a15f9a8bee243580e972cdc
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-03-29 09:23:33 +0300

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+# a1 = int(input("Введите первый элемент арифметической прогрессии: "))
+# d = int(input("Введите разность арифметической прогрессии: "))
+# n = int(input("Введите кол-во элементов арифметической прогрессии: "))
+# array = []
+# for i in range(n):
+#     array.append(a1 + (i - 1) * d)
+# print(array)
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
